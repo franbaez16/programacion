1fca15a04da8cdf34efd39c4833c89ccc15e044d
franbaez16 <103434629+franbaez16@users.noreply.github.com>
2022-04-21 22:26:15 -0300

    Add files via upload

---
+++ logica.js
@@ -0,0 +1,10 @@
+let nombre = "Francesca"
+
+let edad = 15;
+console.log(edad)
+prompt("hola")
+alert("hola2")
+let mensaje = prompt("escriba un numero")
+console.log(mensaje);
+nombre = 20;
+console.log(nombre)
